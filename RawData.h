41c49f5a809939a854f007c5c07eeeae5df5da09
Ivaylo <ivaylo.georgiev18@gmail.com>
2018-06-04 15:23:05 +0300

    Create RawData.h

---
+++ RawData.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+
+/*
+	Stores the content of the .svg file line by line.
+*/
+
+class RawData
+{
+public:
+	RawData();
+	RawData(const char * adress);
+	RawData(const RawData & rhs);
+	RawData& operator=(const RawData & rhs);
+	~RawData();
+
+	std::string * GetRawTags() const { return m_RawTags; }
+	int GetLines()const { return m_Lines; }
+	
+	void AddTag(std::string token);
+	void RemoveTag(int line);
+	int GetLastShapeIndex() const;
+
+	void Print()const;
+
+private:
+	int m_Lines;
+	std::string * m_RawTags;
+};
