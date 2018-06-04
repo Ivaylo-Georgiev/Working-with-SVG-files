7cc735b1f7a4149ed78d762234a75b579255b3c8
Ivaylo <ivaylo.georgiev18@gmail.com>
2018-06-04 20:35:29 +0300

    Update Line.h

--- Line.h
+++ Line.h
@@ -13,7 +13,12 @@ public:
 	std::string GetX2() const { return m_x2; }
 	std::string GetY1() const { return m_y1; }
 	std::string GetY2() const { return m_y2; }
-	//todo: std::string GenerateTag();
+	std::string GetStroke() const { return m_Stroke; }
+
+	void ChangeX1(std::string change) { m_x1 = change; }
+	void ChangeX2(std::string change) {	m_x2 = change; }
+	void ChangeY1(std::string change) { m_y1 = change; }
+	void ChangeY2(std::string change) { m_y2 = change; }
 
 private:
 	std::string m_x1, m_y1, m_x2, m_y2;
