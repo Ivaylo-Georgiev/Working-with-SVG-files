f572527922f7d930388a1c2f2e0507c732608ac6
Ivaylo <ivaylo.georgiev18@gmail.com>
2018-06-08 21:20:53 +0300

    Update Rect.h
    comments added

--- Rect.h
+++ Rect.h
@@ -1,14 +1,23 @@
+/*
+	Defines an abstraction for rectangle.
+*/
+
 #pragma once
 #include "Shape.h"
 
 class Rect : public Shape
 {
 public:
+	//default constructor
 	Rect();
+	//constructor with parameters
 	Rect(std::string tag, int line);
 
+	//ancillary functions
 	void Print() const override;
-	Shape* Clone() const { return new Rect(*this); }
+	Shape* Clone() const override{ return new Rect(*this); }
+
+	//selectors
 	std::string GetX() const { return m_x; }
 	std::string GetY() const { return m_y; }
 	std::string GetHeight() const { return m_Height; }
@@ -16,6 +25,7 @@ public:
 	std::string GetStroke() const { return m_Stroke; }
 	std::string GetFill() const { return m_Fill; }
 
+	//mutators
 	void ChangeX(std::string change) { m_x = change; }
 	void ChangeY(std::string change) { m_y = change; }
 private:
