7be901192a103eeaaa5eed77625b203252aee3f8
Ivaylo <ivaylo.georgiev18@gmail.com>
2018-06-08 21:17:02 +0300

    Update Circle.h
    comments added

--- Circle.h
+++ Circle.h
@@ -1,20 +1,30 @@
+/*
+	Defines an abstraction of circle.
+*/
+
 #pragma once
 #include "Shape.h"
 
 class Circle : public Shape
 {
 public:
+	//default constructor
 	Circle();
+	//constructor with parameters
 	Circle(std::string tag, int line);
 
+	//ancillary unctions
 	void Print() const override;
-	Shape* Clone() const { return new Circle(*this); }
+	Shape* Clone() const override{ return new Circle(*this); }
+
+	//selectors
 	std::string GetCX() const { return m_cx; }
 	std::string GetCY() const { return m_cy; }
 	std::string GetR() const { return m_r; }
 	std::string GetStroke() const { return m_Stroke; }
 	std::string GetFill() const { return m_Fill; }
 
+	//mutators
 	void ChangeCX(std::string change) { m_cx = change; }
 	void ChangeCY(std::string change) { m_cy = change; }
 private:
