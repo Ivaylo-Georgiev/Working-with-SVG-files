e5294749e10c3febc1b18c5e7927e1909dbc9976
Ivaylo <ivaylo.georgiev18@gmail.com>
2018-06-08 21:19:40 +0300

    Update RawData.h
    comments added + save functions

--- RawData.h
+++ RawData.h
@@ -1,31 +1,44 @@
+/*
+	Defines a container abstraction.
+	Stores the content of the .svg file line by line.
+	NB: I assume that every line in the .svg file is a tag.
+*/
+
 #pragma once
 #include <string>
 #include "Shape.h"
 
-/*
-	Stores the content of the .svg file line by line.
-*/
-
 class RawData
 {
 public:
+	//---canonical form---
+	//default constructor
 	RawData();
-	RawData(const char * adress);
+	//constructor with parameters
+	RawData(std::string adress);
+	//copy constructor
 	RawData(const RawData & rhs);
+	//operator overload
 	RawData& operator=(const RawData & rhs);
+	//destructor
 	~RawData();
 
-	std::string * GetRawTags() const { return m_RawTags; }
+	//selectors
 	int GetLines()const { return m_Lines; }
+	std::string * GetRawTags() const { return m_RawTags; }
+	std::string GetAdress() const { return m_Adress; }
+
+	void Save() const;
+	void SaveAs(std::string path) const;
 	
+	//ancillary functions
 	void AddTag(std::string token);
 	void RemoveTag(int line);
 	int GetLastShapeIndex() const;
 	void ReplaceTag(int line, Shape* shape);
-
 	void Print()const;
-
 private:
 	int m_Lines;
 	std::string * m_RawTags;
+	std::string m_Adress;
 };
