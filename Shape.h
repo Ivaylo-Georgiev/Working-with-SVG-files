5cce5853ec279a1a485baffa888e4b04440d11b6
Ivaylo <ivaylo.georgiev18@gmail.com>
2018-06-08 21:22:07 +0300

    Update Shape.h
    comments added

--- Shape.h
+++ Shape.h
@@ -1,19 +1,32 @@
+/*
+	Defines an abstraction for a shape.
+*/
+
 #pragma once
 #include <iostream>
 
+//each shape has it's unique ID
 static int ID = 1;
 
-class Shape
+/*abstract*/ class Shape
 {
 public:
+	//default constructor
 	Shape();
+	//constructor with parameters
 	Shape(std::string tag, int line);
+	//copy constructor
 	Shape(const Shape & rhs) = default;
+	//operator overload
 	Shape& operator=(const Shape & rhs) = default;
+	//VIRTUAL destructor to avoid undefined behavior
 	virtual ~Shape() = default;
 
+	//selectors
 	int GetID()const { return m_ID; }
 	int GetCorrespondingLine()const { return m_CorrespondingLine; }
+
+	//ancillary functions
 	void DecreaseLine() { --m_CorrespondingLine; }
 	virtual void Print() const = 0; //pure virtual function
 	virtual Shape* Clone() const = 0; //pure virtual function
