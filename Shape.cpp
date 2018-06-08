7e22342a1c7dbfd9b3a22819ca5c587fcebb6b44
Ivaylo <ivaylo.georgiev18@gmail.com>
2018-06-08 21:21:35 +0300

    Update Shape.cpp
    comments added

--- Shape.cpp
+++ Shape.cpp
@@ -1,15 +1,17 @@
 #include "Shape.h"
 
-
+//default constructor
 Shape::Shape()
 {
 	m_ID = 0;
 	m_CorrespondingLine = 0;
 }
 
+//constructor with parameters
 Shape::Shape(std::string tag, int line)
 {
 	m_ID = ID;
 	++ID;
 	m_CorrespondingLine = line;
 }
+
